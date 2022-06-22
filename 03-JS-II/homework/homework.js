a3f92306fedbf7044a15eb16785769b39624c171
Ethan <ethanoss311002@gmail.com>
2022-06-22 14:24:16 -0500

    m13

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -79,7 +79,7 @@ function colors(color) {
       case "orange":
       return "This is orange";
       default:
-        "Color not found";
+        return "Color not found";
    }
 
 }
@@ -131,7 +131,7 @@ function fizzBuzz(numero) {
   else if (numero%5===0){
     return "buzz";
   }
-  else if (numero%3===0 && numero%5===0){
+  else if (numero%15===0){
     return "fizzbuzz";
   }
   else{
